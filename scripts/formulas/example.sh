ffbf215e5a0ca803410d9f53942b70a6ab96ed19
Christopher Baker <me@christopherbaker.net>
2019-06-05 12:49:15 -0600

    Initial scripts.

---
+++ scripts/formulas/example.sh
@@ -0,0 +1,54 @@
+#! /bin/bash
+#
+# example
+#
+# notes on build system, reference links, etc
+
+# array of build types supported by this formula
+# you can delete this to implicitly support *all* types
+# FORMULA_TYPES=( "android" "emscripten" "ios" "tvos" "linux" "linuxarmv6" "linuxarmv7" "msys2" "osx" "vs" )
+
+# VER=2.4.0
+
+# download the source code and unpack it into LIB_NAME
+function download() {
+	:
+	# local filename=v${VER}
+	# curl -LO https://github.com/JuliaStrings/utf8proc/archive/${filename}.tar.gz
+	# tar -xf ${filename}.tar.gz
+	# mv utf8proc-$VER utf8proc
+	# rm ${filename}.tar.gz
+}
+
+# prepare the build environment, executed inside the lib src dir
+function prepare() {
+	:
+}
+
+# executed inside the lib src dir
+function build() {
+	:
+	# make
+}
+
+# executed inside the lib src dir, first arg $1 is the dest libs dir root
+function copy() {
+	:
+	# mkdir -p $1/include/
+	# cp -v utf8proc.h $1/include/
+
+	# mkdir -p $1/lib/$TYPE/
+	# cp -v libutf8proc.a $1/lib/$TYPE/
+
+	# # copy license file
+	# rm -rf $1/license # remove any older files if exists
+	# mkdir -p $1/license
+	# cp -v LICENSE.md $1/license/
+	# cp -v README.md $1/license/
+
+}
+
+# executed inside the lib src dir
+function clean() {
+	echo "Nothing to clean."
+}
---
+++ scripts/linux64/install.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+set -e
+
+echo "Installing linux64 specific packages ... "

038706e9c7edab9ec91b9151e94bacb493d8c7b2
Christopher Baker <me@christopherbaker.net>
2019-06-05 14:40:02 -0600

    New examples.

---
+++ examples/category_1/example_1/src/ofApp.cpp
@@ -0,0 +1,16 @@
+//
+// Copyright (c) 2019 Christopher Baker <https://christopherbaker.net>
+//
+// SPDX-License-Identifier:	MIT
+//
+
+
+#include "ofApp.h"
+
+
+void ofApp::setup()
+{
+    ofLog("ofApp::setup") << "Nothing to see.";
+
+    ofExit();
+}
